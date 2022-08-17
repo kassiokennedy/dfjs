62e6e60d7a06404b4318f1c6d083a15e152a7044
kassio kennedy <kassio1206@hotmail.com>
2022-08-17 18:49:22 -0300

    oi

---
+++ script.js
@@ -0,0 +1,30 @@
+// Função auto invocável
+/**
+const soma3  = (
+  function() {
+    return a + b;
+  }
+)(1,2);
+console.log(soma3)
+
+
+// Função Callbacks
+
+// Função anônima
+const soma = function (a, b) {
+  return a + b
+}
+console.log(soma(1, 2))
+
+
+// arrow function
+const soma2 = (a,b) => a + b;
+
+console.log(soma(54,45));
+ */
+const alunos = [
+  { nome: 'Joao', nota: 10 },
+  { nome: 'Sofia', nota: 9 },
+  { nome: 'paulo', nota: 6 }
+]
+function alunosAprovados(array, media) {}
